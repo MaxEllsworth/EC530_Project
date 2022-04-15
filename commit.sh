284891905f16265c3a05eac9b340eef0c5b68b3c
MaxEllsworth <maxell@bu.edu>
2022-04-15 10:19:07 -0400

    Fri 15 Apr 2022 10:19:07 AM EDT

--- commit.sh
+++ commit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-git pull origin user_modules
 echo "#`date -d @$(date +%s)`" >> pushes.py
 git add .
 git commit -m "`date -d @$(date +%s)`"
-git push origin main:user_modules
+git push origin main
