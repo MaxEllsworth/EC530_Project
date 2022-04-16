cdb2b946fca0d93d24eb5d74f003ff8bba188095
MaxEllsworth <maxell@bu.edu>
2022-04-16 16:00:20 -0400

    Sat 16 Apr 2022 04:00:20 PM EDT

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 import template
-import variable
+import variables
 '''
 Types of users:
 	patients
@@ -12,11 +12,24 @@ class user_metaclass(type):
 	def __new__(class_name, what, bases=None, dict=None):
 		return type.__new__(class_name,what,bases,dict)
 
+def add_user(user_type, user_info, uid = ""): 
+	traits = template.load_template(user_type)
+	user = type("user", (user_metaclass,),traits)
+	print(user.identity)
 
-def add_user(user_type): 
+def type_fixer(field, out_type):
+	out = None
+	if (out_type == "str"):
+		out = str(field)
+	if (out_type == "int"):
+		try:
+			out = int(field)
+	if (out_type == "float"):
+		try:
+			out = float(field)
+	return out
+def field_merger(schema, data):
 	True
 
-
-
 if __name__ == "__main__":
-	template.load_template("patient")	
+	add_user("patient")
