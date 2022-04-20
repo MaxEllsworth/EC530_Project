c7b13f0df743c173545f926f92f527efd36773f7
MaxEllsworth <maxell@bu.edu>
2022-04-20 15:06:02 -0400

    Wed 20 Apr 2022 03:06:02 PM EDT

--- src/speech-queue.py
+++ src/speech-queue.py
@@ -5,10 +5,11 @@ import time
 import speech_recognition as sr
 from speech import audio_file
  
-def queue_transcribe_chunks(msgQueue, af):
-    af.transcribe_chunks(transcribe_all = True, chunk_index = 2)
-
-    msgQueue.put("Added at " + str(time.time()))
+def queue_transcribe_chunks(msgQueue, af, chunk_index):
+    chunk_text = af.transcribe_chunks(transcribe_all = False, chunk_index = chunk_index)
+    print("chunk text is " + chunk_text)
+    msgQueue.put(str(chunk_text))
+    time.sleep(1)
     
 # Producer/Writer
 
@@ -25,11 +26,8 @@ def procFunction0(messageQueue):
 # Consumer/Reader
 
 def procFunction1(messageQueue):
-
     while messageQueue.empty() is False:
-
         print("From reader:%s"%messageQueue.get())
-
         time.sleep(1)
 
  
@@ -46,9 +44,17 @@ def procFunction2(messageQueue):
 
  
 
-if __name__ != "__main__":
+if __name__ == "__main__":
 
  
+    r = sr.Recognizer()
+    audio_f = audio_file(audio_uid =  "f5ce9b47903c4e3099ca71ca6a8e67f1",)
+
+    audio_f.audio_file_locator(audio_f.audio_uid)
+    audio_f.chunkify_using_silence()
+    print("this code is so messed up")
+
+
 
     multiprocessing.set_start_method("fork")
 
@@ -60,11 +66,11 @@ if __name__ != "__main__":
 
     # Create child processes
 
-    childProcess0 = multiprocessing.Process(target=queue_transcribe_chunks, args=(messageQueue,))
+    childProcess0 = multiprocessing.Process(target=queue_transcribe_chunks, args=(messageQueue,audio_f, 0))
 
     childProcess1 = multiprocessing.Process(target=procFunction1, args=(messageQueue,))
 
-    childProcess2 = multiprocessing.Process(target=procFunction2, args=(messageQueue,))
+ #   childProcess2 = multiprocessing.Process(target=procFunction2, args=(messageQueue,))
 
  
 
@@ -74,7 +80,7 @@ if __name__ != "__main__":
 
     childProcess1.start()
 
-    childProcess2.start()
+  #  childProcess2.start()
 
  
 
@@ -84,15 +90,9 @@ if __name__ != "__main__":
 
     childProcess1.join()
 
-    childProcess2.join()
-
-if __name__ == "__main__":
-    r = sr.Recognizer()
-    audio_f = audio_file(audio_uid =  "f5ce9b47903c4e3099ca71ca6a8e67f1",)
-
-    audio_f.audio_file_locator(audio_f.audio_uid)
-    audio_f.chunkify_using_silence()
-    print("this code is so messed up")
+ #   childProcess2.join()
 
     print(audio_f.chunk_translations)
+if __name__ != "__main__":
+    True
   #  sleep(100000)
